fcd9f2cb48cdbc91346e4d06809f15c1defe88d9
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-02-22 17:11:54 +0100

    Add more tests

--- stringLength.js
+++ stringLength.js
@@ -1,14 +1,10 @@
 const stringLength = (str) => {
-  return str.length;
+  const leng = str.length;
+  if (leng >= 1 && leng <= 10) {
+    return leng;
+  } else {
+    throw 'The string is less then 1 char or more then 10 characters';
+  }
 };
-
-// const stringLength = (str) => {
-//   const leng = str.length;
-//   if (leng >= 1 && leng <= 10) {
-//     return leng;
-//   } else {
-//     throw 'The string is less then 1 char or more then 10 characters';
-//   }
-// };
 
 module.exports = stringLength;
--- stringLength.test.js
+++ stringLength.test.js
@@ -3,3 +3,15 @@ const stringLength = require('./stringLength');
 test('Return the string lenght', () => {
   expect(stringLength('ismail')).toBe(6);
 });
+
+test('Throw error if the string is less then 1 characters', () => {
+  expect(() => {
+    stringLength('ismailcourr');
+  }).toThrow('The string is less then 1 char or more then 10 characters');
+});
+
+test('Throw error if the string is More then 10 characters', () => {
+  expect(() => {
+    stringLength('ismailcourr');
+  }).toThrow('The string is less then 1 char or more then 10 characters');
+});
