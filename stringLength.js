43a358ab461a3f8315fd9460dfef614c96d75c4b
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-02-22 17:14:45 +0100

    remove else

--- stringLength.js
+++ stringLength.js
@@ -2,9 +2,8 @@ const stringLength = (str) => {
   const leng = str.length;
   if (leng >= 1 && leng <= 10) {
     return leng;
-  } else {
-    throw 'The string is less then 1 char or more then 10 characters';
   }
+  throw 'The string is less then 1 char or more then 10 characters';
 };
 
 module.exports = stringLength;
