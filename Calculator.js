a39ca6e3f75c5a02a174258a78ef72203d3dcaf9
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-02-22 19:13:01 +0100

    Fix Linters

--- Calculator.js
+++ Calculator.js
@@ -1,23 +1,27 @@
 class Calculator {
   constructor() {
-    this.numOne;
-    this.numTwo;
+    this.numOne = 1;
+    this.numTwo = 1;
   }
+
   add(numOne, numTwo) {
     this.numOne = numOne;
     this.numTwo = numTwo;
     return this.numOne + this.numTwo;
   }
+
   subtract(numOne, numTwo) {
     this.numOne = numOne;
     this.numTwo = numTwo;
     return this.numOne - this.numTwo;
   }
+
   divide(numOne, numTwo) {
     this.numOne = numOne;
     this.numTwo = numTwo;
     return this.numOne / this.numTwo;
   }
+
   multiply(numOne, numTwo) {
     this.numOne = numOne;
     this.numTwo = numTwo;
