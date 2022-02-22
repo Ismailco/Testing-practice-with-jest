d175b9e62493d4e947b2b1f56a15d796e024de58
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-02-22 16:55:24 +0100

    add first test

---
+++ stringLength.js
@@ -0,0 +1,14 @@
+const stringLength = (str) => {
+  return str.length;
+};
+
+// const stringLength = (str) => {
+//   const leng = str.length;
+//   if (leng >= 1 && leng <= 10) {
+//     return leng;
+//   } else {
+//     throw 'The string is less then 1 char or more then 10 characters';
+//   }
+// };
+
+module.exports = stringLength;
---
+++ stringLength.test.js
@@ -0,0 +1,5 @@
+const stringLength = require('./stringLength');
+
+test('Return the string lenght', () => {
+  expect(stringLength('ismail')).toBe(6);
+});
