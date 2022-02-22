5d50a62261868d0de55a4c0c0ceeb7293678fd16
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-02-22 19:05:51 +0100

    Fix Linters

--- reverseString.js
+++ reverseString.js
@@ -1,6 +1,6 @@
 const reverseString = (str) => {
   let newStr = '';
-  for (let i = str.length - 1; i >= 0; i--) {
+  for (let i = str.length - 1; i >= 0; i - 1) {
     newStr += str[i];
   }
   return newStr;
